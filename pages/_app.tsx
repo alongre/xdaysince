fa92b2a78d62f4d787ce0642817872614e51916e
alongre <alongre@gmail.com>
2021-08-22 07:54:12 +0300

    added next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/globals.css';
 import Nav from './components/nav';
+import { Provider } from 'next-auth/client';
 // import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<div className='container h-screen m-auto bg-green-500'>
-			<Nav />
-			<div className='flex  justify-center items-center h-screen text-center'>
-				<Component {...pageProps} />
+		<Provider session={pageProps.session}>
+			<div className='container h-screen m-auto bg-green-500'>
+				<Nav />
+				<div className='flex  justify-center items-center h-screen text-center'>
+					<Component {...pageProps} />
+				</div>
 			</div>
-		</div>
+		</Provider>
 	);
 }
 
--- pages/components/nav.tsx
+++ pages/components/nav.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 const Nav = () => {
+	const [session, loading] = useSession();
 	return (
 		<ul className='flex m-2'>
 			<li className='mr-6'>
@@ -17,6 +19,20 @@ const Nav = () => {
 				<Link href='profile'>
 					<a className='text-blue-500 hover:text-blue-800'>Profile</a>
 				</Link>
+			</li>
+			<li>
+				{!session && (
+					<>
+						Not signed in <br />
+						<button onClick={() => signIn()}>Sign in</button>
+					</>
+				)}
+				{session && (
+					<>
+						Signed in as {session.user.email} <br />
+						<button onClick={() => signOut()}>Sign out</button>
+					</>
+				)}
 			</li>
 		</ul>
 	);
